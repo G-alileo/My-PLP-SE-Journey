4fc89528deb1930a13bd169a6dc9c0f001f33e32
G-alileo <jamespmurithi@gmail.com>
2025-07-17 06:39:21 +0300

    Feat : New modules.py source file
    Illustration of how python modules work and how to use them
    In the above example we use the random module

---
+++ Week 1/modules.py
@@ -0,0 +1,7 @@
+# Python comes with a toolkit of modules—specialized code libraries that you can use without building everything from scratch. It’s like using a toolbox instead of crafting tools by hand! 🔨
+# To use a module, just import it:
+import random
+
+# Generate a random number between 1 and 6 (like rolling a die)
+dice_roll = random.randint(1, 6)
+print("You rolled a", dice_roll)
