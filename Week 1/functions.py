67a5d33efaa65a99bba8ffa67c5061be175416be
G-alileo <jamespmurithi@gmail.com>
2025-07-17 06:35:18 +0300

    Feat : New functions.py file
    Functions are blocks of reusable code—they save you from repeating the same code over and over.

---
+++ Week 1/functions.py
@@ -0,0 +1,11 @@
+# Defining a function - 1
+def add_numbers(x, y):
+    return x + y
+
+# Defining a function - 2
+def greet(name):
+    print("Hello, " + name + "! 👋")
+
+# Calling the function
+print("Sum of 5 and 3 is:", add_numbers(5, 3))  # Outputs: 8
+greet("Alice")   # Outputs: Hello, Alice! 👋 
