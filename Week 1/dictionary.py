85820cec49503fb15ba47928cb10e56b1f3b488d
G-alileo <jamespmurithi@gmail.com>
2025-07-17 06:31:24 +0300

    Feat : New dictionary.py source file
     A dictionary is like a phonebook—it stores key-value pairs.

---
+++ Week 1/dictionary.py
@@ -0,0 +1,11 @@
+student_info = {"name": "Charlie", "age": 21, "grade": "A"}
+
+# This is a simple dictionary to hold student information
+print("Student Information:", student_info)  # Outputs: {'name': 'Charlie', 'age': 21, 'grade': 'A'}
+
+# Accessing a value by its key
+print("Student Name : ",student_info["name"])  # Outputs: Charlie
+
+# Modifying a value
+student_info["age"] = 22
+print("Modified student info  : ",student_info)  # Outputs: {'name': 'Charlie', 'age': 22, 'grade': 'A'}
