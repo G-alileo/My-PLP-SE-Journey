17307bafdd2004c669484b4a9e559257acae030b
G-alileo <jamespmurithi@gmail.com>
2025-07-17 06:12:50 +0300

    Feat : New lists.py source file
    A list is like a collection of items.This file shows basic operations on lists

---
+++ Week 1/lists.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry"]
+
+# This is a simple list of fruits
+print("Original list of fruits:", fruits)  # Outputs: ['apple', 'banana', 'cherry']
+
+# Accessing elements
+print("First item in the list : ", fruits[0])  # Outputs: apple
+
+# Modifying an element
+fruits[1] = "orange"
+print("Modifying the second element in the list : ",fruits)  # Outputs: ['apple', 'orange', 'cherry']
+
+# Adding an element
+fruits.append("grape")
+print("Adding an item to the list: ",fruits)  # Outputs: ['apple', 'orange', 'cherry', 'grape']
