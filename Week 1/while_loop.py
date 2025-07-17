6bc95816c9890e219381d7b97c32c6a8344ad437
G-alileo <jamespmurithi@gmail.com>
2025-07-17 06:06:45 +0300

    Feat : Implementation of Taking user input
     Allowing the user to enter their own custom count down numeric value

--- Week 1/while_loop.py
+++ Week 1/while_loop.py
@@ -1,4 +1,6 @@
-countdown = 5
+countdown = int(input("Enter a countdown number: "))
+# This will count down from the number provided by the user to 0
+print("Starting countdown... 🚀")
 
 while countdown > 0:
     print("Counting down:", countdown)
